aa8d358e11803c2961a2d22651a370eff8901fae
Wesley <wesmith314@gmail.com>
2024-03-16 23:11:27 -0700

    formats navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import Link from 'next/link';
+import { Button } from 'semantic-ui-react';
 
 function NavBar() {
   return (
-    <div>
-      
+    <div className="navbar">
+      <Link href='/'>
+        <Button className='menu-btn'>
+          Home
+        </Button>
+      </Link>
+      <Link href='/about'>
+        <Button className='menu-btn'>
+          About Us
+        </Button>
+      </Link>
+      <Link href='/services'>
+        <Button className='menu-btn'>
+          Services
+        </Button>
+      </Link>
+      <Link href='/portfolio'>
+        <Button className='menu-btn'>
+          Portfolio
+        </Button>
+      </Link>
+      <Link href='/projects'>
+        <Button className='menu-btn'>
+          Projects
+        </Button>
+      </Link>
+      <Link href='/contact'>
+        <Button className='menu-btn'>
+          Contact
+        </Button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
