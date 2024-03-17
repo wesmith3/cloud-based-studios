5bd3e0709ce10443c90502e60d45a081f00b98b8
Wesley <wesmith314@gmail.com>
2024-03-16 23:03:48 -0700

    installs semantic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'semantic-ui-css/semantic.min.css'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
