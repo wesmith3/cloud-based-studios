91beb5036a0751290385b58ae5a1e36d4fc271a9
Wesley <wesmith314@gmail.com>
2024-03-16 23:09:18 -0700

    adds navbar file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NavBar from './components/navbar'
 import 'semantic-ui-css/semantic.min.css'
 import "./globals.css";
 
